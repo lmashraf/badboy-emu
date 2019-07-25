9340406e190193d7c5ced330878492179688330b
Lebrazi, MohammedAchraf <mohammedachraf.lebrazi@here.com>
2019-07-25 15:27:33 +0200

    Added R/W for virtual 16-bits registers (ie. BC, AF, DE and HL)

--- src/core/registers.rs
+++ src/core/registers.rs
@@ -29,16 +29,53 @@ impl Registers
             L: 0,
         }
     }
-    // Read from virtual 16-bits register
+
+    // Read/Write for virtual 16-bits registers
+    // BC:
     fn get_BC(&self) -> u16
     {
         (self.B as u16) << 8 | (self.C as u16)
     }
 
-    // Write to virtual 16-bits register
     fn set_BC(&mut self, value: u16)
     {
         self.B = ((value & 0xFF00) >> 8) as u8;
         self.C = (value & 0xFF) as u8;
+    }
+
+    // AF:
+    fn get_AF(&self) -> u16
+    {
+        (self.A as u16) << 8 | (self.F as u16)
+    }
+
+    fn set_AF(&mut self, value: u16)
+    {
+        self.A = ((value & 0xFF00) >> 8) as u8;
+        self.F = FlagsRegister::from((value & 0xFF) as u8);
+    }
+
+    // DE:
+    fn get_DE(&self) -> u16
+    {
+        (self.D as u16) << 8 | (self.E as u16)
+    }
+
+    fn set_DE(&mut self, value: u16)
+    {
+        self.D = ((value & 0xFF00) >> 8) as u8;
+        self.E = (value & 0xFF) as u8;
+    }
+
+    // HL:
+    fn get_HL(&self) -> u16
+    {
+        (self.H as u16) << 8 | (self.L as u16)
+    }
+
+    fn set_HL(&mut self, value: u16)
+    {
+        self.H = ((value & 0xFF00) >> 8) as u8;
+        self.L = (value & 0xFF) as u8;
     }
 }
