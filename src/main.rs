ff82606fc1cd3bbd3a42fcae3ed91c92886b2fad
Lebrazi, MohammedAchraf <mohammedachraf.lebrazi@here.com>
2019-07-26 10:34:17 +0200

    Updated entry point with some infos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// Window Constants
+const ENLARGEMENT_FACTOR: usize = 1
+const WINDOW: [usize; 2] = [(160 * ENLARGEMENT_FACTOR), (144 * ENLARGEMENT_FACTOR)];
+const NUMBER_OF_PIXELS: usize = 23040;
+
+// Op Cycle
+const ONE_SECOND_IN_MICROS: usize = 1000000000;
+const ONE_SECOND_IN_CYCLES: usize = 4190000;
+const ONE_FRAME_IN_CYCLES: usize = 70224;
+
+fn main()
+{
+    // TODO:
+    // - initialise emulator App
+    // - initialise boot buffer and game buffer
+    // - initialise CPU: boot/game buffer
+    // - initialise Window: dimentions, options, title, etc.
+    // - run emulation cycle
 }
+
+fn run()
+{}
